17caa676008113cf6f8ab5387b9530bd9a4aa171
beatcracker <beatcracker@users.noreply.github.com>
2017-06-22 19:15:17 +0300

    Remove unused resource

--- FirewatchFOVChanger/MainForm.Designer.cs
+++ FirewatchFOVChanger/MainForm.Designer.cs
@@ -30,44 +30,32 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.fovLabel = new System.Windows.Forms.Label();
-            this.pbLogo = new System.Windows.Forms.PictureBox();
             this.defaultButton = new System.Windows.Forms.Button();
             this.minLabel = new System.Windows.Forms.Label();
             this.maxLabel = new System.Windows.Forms.Label();
             this.applyButton = new System.Windows.Forms.Button();
             this.fovUpDown = new System.Windows.Forms.NumericUpDown();
             this.fovTrackBar = new System.Windows.Forms.TrackBar();
-            ((System.ComponentModel.ISupportInitialize)(this.pbLogo)).BeginInit();
+            this.pbLogo = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.fovUpDown)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.fovTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLogo)).BeginInit();
             this.SuspendLayout();
             // 
             // fovLabel
             // 
             this.fovLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.fovLabel.Location = new System.Drawing.Point(88, 167);
+            this.fovLabel.Location = new System.Drawing.Point(88, 166);
             this.fovLabel.Name = "fovLabel";
             this.fovLabel.Size = new System.Drawing.Size(143, 26);
             this.fovLabel.TabIndex = 7;
             this.fovLabel.Text = "Current FOV: ??.  New FOV:";
             this.fovLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
-            // pbLogo
-            // 
-            this.pbLogo.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.pbLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.pbLogo.Image = global::FirewatchFOVChanger.Properties.Resources.Banner;
-            this.pbLogo.Location = new System.Drawing.Point(12, 12);
-            this.pbLogo.Name = "pbLogo";
-            this.pbLogo.Size = new System.Drawing.Size(350, 98);
-            this.pbLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.pbLogo.TabIndex = 4;
-            this.pbLogo.TabStop = false;
-            // 
             // defaultButton
             // 
             this.defaultButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.defaultButton.Location = new System.Drawing.Point(12, 168);
+            this.defaultButton.Location = new System.Drawing.Point(12, 167);
             this.defaultButton.Name = "defaultButton";
             this.defaultButton.Size = new System.Drawing.Size(75, 25);
             this.defaultButton.TabIndex = 8;
@@ -97,7 +85,7 @@
             // applyButton
             // 
             this.applyButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.applyButton.Location = new System.Drawing.Point(287, 168);
+            this.applyButton.Location = new System.Drawing.Point(287, 167);
             this.applyButton.Name = "applyButton";
             this.applyButton.Size = new System.Drawing.Size(75, 25);
             this.applyButton.TabIndex = 10;
@@ -108,7 +96,7 @@
             // fovUpDown
             // 
             this.fovUpDown.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.fovUpDown.Location = new System.Drawing.Point(232, 171);
+            this.fovUpDown.Location = new System.Drawing.Point(232, 170);
             this.fovUpDown.Maximum = new decimal(new int[] {
             110,
             0,
@@ -140,11 +128,23 @@
             this.fovTrackBar.TabIndex = 12;
             this.fovTrackBar.Value = 55;
             // 
+            // pbLogo
+            // 
+            this.pbLogo.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.pbLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pbLogo.Image = global::FirewatchFOVChanger.Properties.Resources.Banner;
+            this.pbLogo.Location = new System.Drawing.Point(12, 12);
+            this.pbLogo.Name = "pbLogo";
+            this.pbLogo.Size = new System.Drawing.Size(350, 98);
+            this.pbLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pbLogo.TabIndex = 4;
+            this.pbLogo.TabStop = false;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(374, 202);
+            this.ClientSize = new System.Drawing.Size(374, 201);
             this.Controls.Add(this.fovUpDown);
             this.Controls.Add(this.applyButton);
             this.Controls.Add(this.maxLabel);
@@ -153,16 +153,16 @@
             this.Controls.Add(this.fovLabel);
             this.Controls.Add(this.pbLogo);
             this.Controls.Add(this.fovTrackBar);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "MainForm";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Firewatch FOV Changer";
-            ((System.ComponentModel.ISupportInitialize)(this.pbLogo)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.fovUpDown)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.fovTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLogo)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- FirewatchFOVChanger/Properties/Resources.Designer.cs
+++ FirewatchFOVChanger/Properties/Resources.Designer.cs
@@ -69,15 +69,5 @@ namespace FirewatchFOVChanger.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap Main {
-            get {
-                object obj = ResourceManager.GetObject("Main", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
